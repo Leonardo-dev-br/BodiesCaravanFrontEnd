31b262f61a91b013b1465595d57b90c0fc8bdc5e
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-01 18:26:04 -0300

    WIP: Adicionando alguma coisa na HOME

--- bodies-caravan/src/app/page.tsx
+++ bodies-caravan/src/app/page.tsx
@@ -2,6 +2,17 @@ import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <Header />
+    <>
+      <Header />
+      <main className="flex justify-center items-center min-h-screen bg-blue-950 text-white">
+        <section className="text-center max-w-lg p-6">
+          <h1 className="text-3xl font-bold">🚀 Explore o espaço sem sair da Terra! 🌍🔭</h1>
+          <p className="text-lg mt-4">
+            Quer saber quais asteroides e cometas estão passeando perto do nosso planeta? Nosso sistema te mantém informado
+            em tempo real. Acompanhe o cosmos e descubra os segredos do universo—antes que ele venha bater na nossa porta! 🌠😆
+          </p>
+        </section>
+      </main>
+    </>
   );
 }
