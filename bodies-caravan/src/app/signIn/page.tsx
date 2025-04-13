366a8a02a0e8ab6b2549d879308e0a2118145c08
Leonardo-dev-br <99846550+Leonardo-dev-br@users.noreply.github.com>
2025-04-13 01:56:25 -0300

    WIP: Criando outras páginas

---
+++ bodies-caravan/src/app/signIn/page.tsx
@@ -0,0 +1,8 @@
+import Header from "../components/Header";
+
+export default function SignIn() {
+    return (
+        <Header/>
+        //FORMULARIO PARA VALIDAR  AQUI
+    );
+}
---
+++ bodies-caravan/src/app/signUp/page.tsx
@@ -0,0 +1,8 @@
+import Header from "../components/Header";
+
+export default function SignUp() {
+    return (
+        <Header/>
+        //FORMULARIO AQUI
+    );
+}
